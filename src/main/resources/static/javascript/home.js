ba29d5500e839090df3523491869fb704837b203
anh-nt24 <theanh24013@gmail.com>
2023-11-15 16:43:06 +0700

    update homepage

--- src/main/resources/static/javascript/home.js
+++ src/main/resources/static/javascript/home.js
@@ -1,13 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
 	var currentPath = window.location.pathname;
+	console.log(currentPath);
 	var navItems = document.querySelectorAll("#home-nav li");
+	console.log(navItems);
 
 	navItems.forEach(function (item) {
 		var link = item.querySelector("a");
-		var href = link.getAttribute("href");
-
-		if (currentPath === href) {
-			item.classList.add("active");
+		if (link) {
+			var href = link.getAttribute("href");
+			console.log(href)
+			if (currentPath === href) {
+				item.classList.add("active");
+			}
 		}
 	});
 });

e8315c946dda47f7bf4fac2c125f01b1cb127b35
Phạm Trần Thảo Nguyên <52100913@student.tdtu.edu.vn>
2023-11-27 00:58:31 +0700

    Update Student UI

--- src/main/java/vn/edu/tdtu/exam/controller/StudentController.java
+++ src/main/java/vn/edu/tdtu/exam/controller/StudentController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/student")
 public class StudentController {
-    @GetMapping("/")
+    @GetMapping("/home")
     public String home() {
         return "student/home";
     }
